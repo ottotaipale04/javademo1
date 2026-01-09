2a3d02abc40a97e5d443074e7d202ba6dfe3a28d
Otto Taipale <otto.taipale04@gmail.com>
2026-01-09 18:37:40 +0200

    Initial commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.print("Tekstiä ");
+        System.out.println("tänne.");
+        System.out.println(666);
+    }
+}
